ae4c8b66a1f10bc727e62c48a73a0c54a7b28765
Vitaly Kuzyaev <vitkuz573@gmail.com>
2022-06-20 11:21:55 +0500

    Update

--- course_project/AquariumForm.cs
+++ course_project/AquariumForm.cs
@@ -6,7 +6,7 @@ namespace course_project
 {
     public partial class AquariumForm : Form
     {
-        Random random = new Random();
+        readonly Random random = new Random();
 
         public PictureBox Aquarium => aquarium_picturebox;
 
@@ -28,28 +28,18 @@ namespace course_project
 
         private void Add_carp_button_Click(object sender, EventArgs e)
         {
-            CarpFlock<int[]> carpFlock = new CarpFlock<int[]>
+            _ = new CarpFlock<int[]>
             {
                 { FishCoordinates, this }
             };
-
-            foreach (var carp in carpFlock)
-            {
-                Console.Write(carp);
-            }
         }
 
         private void Add_pike_button_Click(object sender, EventArgs e)
         {
-            PikeFlock<int[]> pikeFlock = new PikeFlock<int[]>
+            _ = new PikeFlock<int[]>
             {
                 { FishCoordinates, this }
             };
-
-            foreach (var pike in pikeFlock)
-            {
-                Console.WriteLine(pike);
-            }
         }
 
         public int[] FishCoordinates

80a1492a1a29cbf64e258633b0e6f9d030f9fb7c
Vitaly Kuzyaev <vitkuz573@gmail.com>
2022-06-22 14:53:46 +0500

    Update

--- course_project/Fish.cs
+++ course_project/Fish.cs
@@ -5,14 +5,37 @@ namespace course_project
     internal class Fish
     {
         private protected Brush brush;
+        private protected Point coordinates;
+        private protected Size size;
+        private protected Size speed;
 
         public Fish(Color color)
         {
             brush = new SolidBrush(color);
+
+            speed = new Size(5, 5);
         }
 
         public virtual void Draw(Graphics graphics) { }
 
         public Point Data { get; set; }
+
+        public void UpdateLocation(Rectangle bounds)
+        {
+            if (!bounds.Contains(coordinates + speed))
+            {
+                if (coordinates.X + speed.Width < bounds.Left || coordinates.X + speed.Width > bounds.Right - size.Width)
+                {
+                    speed.Width *= -1;
+                }
+
+                if (coordinates.Y + speed.Height < bounds.Top || coordinates.Y + speed.Height > bounds.Bottom - size.Height)
+                {
+                    speed.Height *= -1;
+                }
+            }
+
+            coordinates += speed;
+        }
     }
 }
--- course_project/Pike.cs
+++ course_project/Pike.cs
@@ -4,15 +4,11 @@ namespace course_project
 {
     internal class Pike : Fish
     {
-        private Point coordinates;
-        private Size size = new Size(40, 40);
-        private Size speed;
-
         public Pike(Point data) : base(Color.Green)
         {
             Data = data;
 
-            speed = new Size(5, 5);
+            size = new Size(40, 40);
         }
 
         public Pike Next { get; set; }
@@ -20,24 +16,6 @@ namespace course_project
         public override void Draw(Graphics graphics)
         {
             graphics.FillEllipse(brush, coordinates.X, coordinates.Y, size.Width, size.Height);
-        }
-
-        public void UpdateLocation(Rectangle bounds)
-        {
-            if (!bounds.Contains(coordinates + speed))
-            {
-                if (coordinates.X + speed.Width < bounds.Left || coordinates.X + speed.Width > bounds.Right - size.Width)
-                {
-                    speed.Width *= -1;
-                }
-
-                if (coordinates.Y + speed.Height < bounds.Top || coordinates.Y + speed.Height > bounds.Bottom - size.Height)
-                {
-                    speed.Height *= -1;
-                }
-            }
-
-            coordinates += speed;
         }
     }
 }

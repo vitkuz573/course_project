ad6b4e06c2e87ea1f515950e392efd34e82bc4cf
Vitaly Kuzyaev <vitkuz573@gmail.com>
2022-06-28 13:29:59 +0500

    update

--- course_project/Fish.cs
+++ course_project/Fish.cs
@@ -6,7 +6,6 @@ namespace course_project
     public class Fish
     {
         protected readonly Random random;
-        protected Point coordinates;
         protected Image image;
         protected Size size;
         protected Size speed;
@@ -14,12 +13,34 @@ namespace course_project
         public Fish()
         {
             random = new Random();
+
+            speed = new Size(random.Next(-5, 5), random.Next(-5, 5));
         }
 
         public Point Data { get; set; }
 
         public virtual void Draw(Graphics graphics)
         {
+        }
+
+        public Point UpdateLocation(Rectangle bounds)
+        {
+            if (!bounds.Contains(Data + speed))
+            {
+                if (Data.X + speed.Width < bounds.Left ||
+                    Data.X + speed.Width > bounds.Right - size.Width)
+                {
+                    speed.Width *= -1;
+                    image.RotateFlip(RotateFlipType.Rotate180FlipY);
+                }
+
+                if (Data.Y + speed.Height < bounds.Top ||
+                    Data.Y + speed.Height > bounds.Bottom - size.Height) speed.Height *= -1;
+            }
+
+            Data += speed;
+
+            return Data;
         }
     }
 }
--- course_project/Fishes/Carp.cs
+++ course_project/Fishes/Carp.cs
@@ -7,11 +7,9 @@ namespace course_project.Fishes
     {
         public Carp(Point data)
         {
-            Data = coordinates = data;
+            Data = data;
 
             size = new Size(40, 40);
-
-            speed = new Size(random.Next(-5, 5), random.Next(-5, 5));
 
             image = speed.Width < 0
                 ? Resources.carp_to_left
@@ -22,23 +20,7 @@ namespace course_project.Fishes
 
         public override void Draw(Graphics graphics)
         {
-            graphics.DrawImage(image, coordinates.X, coordinates.Y, size.Width, size.Height);
-        }
-
-        public Point UpdateLocation(Rectangle bounds)
-        {
-            if (!bounds.Contains(coordinates + speed))
-            {
-                if (coordinates.X + speed.Width < bounds.Left ||
-                    coordinates.X + speed.Width > bounds.Right - size.Width) speed.Width *= -1;
-
-                if (coordinates.Y + speed.Height < bounds.Top ||
-                    coordinates.Y + speed.Height > bounds.Bottom - size.Height) speed.Height *= -1;
-            }
-
-            coordinates += speed;
-
-            return coordinates;
+            graphics.DrawImage(image, Data.X, Data.Y, size.Width, size.Height);
         }
     }
 }
